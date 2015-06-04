fc17a836c058ecdaba95463c38e404983e22beb0
Photomotch <info@aflame.co.jp>
2015-06-04 19:27:33 +0900

    change test.bash for same.py

--- test.bash
+++ test.bash
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo "This is a Test."
-echo "It's something new!!"
-echo "Good bye!"
-
-exit 10
+tmp=/tmp/$$
+ERROR_EXIT () {
+echo "$1" >&2
+rm -f $tmp-*
+exit 1
+}
+#####################################
+# TEST1
+#####################################
+echo aaa bbb NG > $tmp-ans
+./same.py aaa bbb > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+echo OK
+rm -f $tmp-*
+exit 0
